a6d5083ab5db3f3b50681ad3c138c711fd829c7f
Clinton Enwerem <enwerem@terpmail.umd.edu>
2025-11-04 02:53:55 -0500

    apache

--- src/ur3_ik_circle.py
+++ src/ur3_ik_circle.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+
+# Lab 2: Manipulator Kinematics with the Robotics Toolbox for Python
+# Copyright (C) 2025 Clinton Enwerem
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import roboticstoolbox as rtb 
 import numpy as np # library containing important kernels for matrix math
 import spatialmath as sm # library containing important kernels for tf math
